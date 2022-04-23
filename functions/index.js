32c192317f2572b98016d810de42f5ef700f1cdf
moffy-Black <black.river.emperor@outlook.jp>
2022-04-24 00:37:06 +0900

    [create]email-users

--- functions/index.js
+++ functions/index.js
@@ -4,8 +4,55 @@
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+const mailTransport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: gmailEmail,
+        pass: gmailPassword,
+    },
 });
+
+const APP_NAME = 'Cloud Storage for Firebase quickstart';
+
+exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
+    const email = user.email;
+    const displayName = user.displayName;
+
+    return sendWelcomeEmail(email, displayName);
+});
+
+exports.sendByeEmail = functions.auth.user().onDelete((user) => {
+    const email = user.email;
+    const displayName = user.displayName;
+
+    return sendGoodbyeEmail(email, displayName);
+});
+
+async function sendWelcomeEmail(email, displayName) {
+    const mailOptions = {
+        from: `${APP_NAME} <noreply@firebase.com>`,
+        to: email,
+    };
+
+    mailOptions.subject = `Welcome to ${APP_NAME}`;
+    mailOptions.text = `Hey ${displayName || ''}! Welcome to ${APP_NAME}. I hope you will enjoy our service.`;
+    await mailTransport.sendMail(mailOptions);
+    functions.logger.log('New welcome email sent to:', email);
+    return null;
+}
+
+async function sendGoodbyeEmail(email, displayName) {
+    const mailOptions = {
+        from: `${APP_NAME} <noreply@firebase.com>`,
+        to: email,
+    };
+    mailOptions.subject = `Bye!`;
+    mailOptions.text = `Hey ${displayName || ''}!, We confirm that we have deleted your ${APP_NAME} account.`;
+    await mailTransport.sendMail(mailOptions);
+    functions.logger.log('Accounts deletion confirmation email sent to:', email);
+    return null;
+}
